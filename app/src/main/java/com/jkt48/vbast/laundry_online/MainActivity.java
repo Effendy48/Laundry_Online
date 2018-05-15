fc263e95cc8d023700eccf19bcd8c03bf980c822
Effendy Wahyu Pradana <wahyupradana729@gmail.com>
2018-05-15 08:41:28 +0700

    Tambah_Form_Pesan
    Pesan pada Aplikasi Laundry Online

--- app/src/main/java/com/jkt48/vbast/laundry_online/MainActivity.java
+++ app/src/main/java/com/jkt48/vbast/laundry_online/MainActivity.java
@@ -1,15 +1,49 @@
 package com.jkt48.vbast.laundry_online;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_Pesan,btn_Tunggu,btn_Bayar;
+    TextView tv_email_view;
+    Intent a;
 
+    String kd,email;
+    SharedPreferences sharedPreferences;
+
+    public static final String TAG_KD = "kd";
+    public static final String TAG_EMAIL = "email";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        sharedPreferences = getSharedPreferences(Login.my_shared_preference, Context.MODE_PRIVATE);
+        kd = getIntent().getStringExtra(TAG_KD);
+        email = getIntent().getStringExtra(TAG_EMAIL);
 
+
+        getSupportActionBar().setTitle(email);
+
+        btn_Pesan = (Button)findViewById(R.id.btn_pesan);
+
+
+        btn_Pesan.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view){
+                Intent klik = new Intent(getApplicationContext(),Pesan.class);
+                klik.putExtra("kd",getIntent().getStringExtra(TAG_KD));
+                klik.putExtra("email",getIntent().getStringExtra(TAG_EMAIL));
+                startActivity(klik);
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/jkt48/vbast/laundry_online/Pesan.java
@@ -0,0 +1,63 @@
+package com.jkt48.vbast.laundry_online;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+public class Pesan extends AppCompatActivity {
+    EditText et_User;
+    TextView tv_Berat,tv_total_bayar;
+    int berat,total_bayar;
+    ImageView img_Tambah,img_Kurang;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pesan);
+        Intent over = getIntent();
+        et_User = (EditText)findViewById(R.id.et_user_pesan);
+        et_User.setText(over.getStringExtra("email"));
+        tv_Berat = (TextView)findViewById(R.id.berat_pesan);
+        tv_total_bayar = (TextView)findViewById(R.id.tv_total_bayar);
+
+
+        img_Tambah = (ImageView)findViewById(R.id.img_tambah);
+        img_Kurang = (ImageView)findViewById(R.id.img_kurang);
+
+
+        berat = Integer.parseInt(tv_Berat.getText().toString());
+
+        img_Tambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int total =  berat ++;
+                int bayar = total * 10000;
+
+                tv_total_bayar.setText(String.valueOf(bayar));
+                tv_Berat.setText(String.valueOf(total));
+            }
+        });
+        img_Kurang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int total_bayar = Integer.parseInt(tv_total_bayar.getText().toString());
+                berat = Integer.parseInt(tv_Berat.getText().toString());
+
+                int kurang = new Integer(berat - 1);
+                tv_Berat.setText(String.valueOf(kurang));
+
+                int bayar = total_bayar - 10000;
+
+                tv_total_bayar.setText(String.valueOf(bayar));
+                tv_Berat.setText(String.valueOf(kurang));
+
+            }
+        });
+
+    }
+}
